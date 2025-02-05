886adca2259378360e66f5f12ab17ad7c4bc0c5d
jgmejilla <jgmejilla@up.edu.ph>
2025-02-05 23:49:13 +0800

    feat(Frontend): Add basic login UI

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  let pages = ['dashboard', 'login', 'search']
+</script>
+
+
+<div class="p-4 flex flex-col space-y-3">
+  <h1 class="text-2xl font-bold text-gray-800">links</h1>
+  <nav class="flex flex-col space-y-2">
+    {#each pages as page}
+      <a href="/{page}" class="text-blue-500 hover:text-blue-700">{page}</a>
+    {/each}
+  </nav>  
+</div> 
---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  let email = '';
+  let password = '';
+  
+  let loginMethods = ['Google', 'Facebook']
+
+  function handleSubmit() {
+    console.log('Login attempted:', { email, password });
+  }
+</script>
+
+
+<div 
+  class="flex min-h-screen items-center justify-center bg-gradient-to-br bg-white"
+>
+  <div class="bg-white w-full max-w-xl space-y-4 rounded-lg border p-16 shadow-xl">
+    <form on:submit|preventDefault={handleSubmit} class="space-y-3">
+      <h1 class="text-3xl font-bold -mb-2 text-emerald-800">Lutong Bahay</h1>
+      <h2 class="text-gray-600 pb-4 italic">Your database for Filipino recipes</h2>
+      <div class="space-y-2">
+        <input
+          type="email"
+          bind:value={email}
+          placeholder="Email"
+          class="w-full rounded border p-2"
+          required
+        />
+      </div>
+    
+      <div class="space-y-2">
+        <input
+          type="password"
+          bind:value={password}
+          placeholder="Password"
+          class="w-full rounded border p-2"
+          required
+        />
+      </div>
+      <button type="submit" class="mt-3 w-full rounded bg-emerald-500 p-2 text-white hover:bg-emerald-600">
+        Log in
+      </button>
+    
+    
+      <div class="flex items-center justify-center pt-5 pb-3">
+        <hr class="flex-grow border-gray-200">
+        <span class="px-4 text-gray-500">OR</span>
+        <hr class="flex-grow border-gray-200">
+      </div>
+    
+    
+    </form>
+
+
+    <div class="space-y-3 flex flex-col items-center ">
+      {#each loginMethods as loginMethod}
+        <button type="submit" class="w-full rounded-full bg-white p-2 text-gray-700 border shadow-sm">
+          Sign in with {loginMethod}
+        </button>
+      {/each}
+    </div>
+  </div>
+
+
+
+</div> 
