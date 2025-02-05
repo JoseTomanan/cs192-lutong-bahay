7bfd3ff5abe596d459f0d12986099068bd538bef
jgmejilla <jgmejilla@up.edu.ph>
2025-02-06 00:07:41 +0800

    feat(Login): change layout

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -13,13 +13,14 @@
 <div 
   class="flex min-h-screen items-center justify-center bg-gradient-to-br bg-white"
 >
-  <div class="bg-white w-full max-w-xl space-y-4 rounded-lg border p-16 shadow-xl">
+  <div class="bg-white w-full flex flex-col h-screen justify-center max-w-xl space-y-3 rounded-lg border px-12 py-12 shadow-xl"
+  style="background: url(/liempo_edited.xpng); background-size:cover">
     <form on:submit|preventDefault={handleSubmit} class="space-y-3">
       <h1 class="text-3xl font-bold -mb-2 text-emerald-800">Lutong Bahay</h1>
       <h2 class="text-gray-600 pb-4 italic">Your database for Filipino recipes</h2>
       <div class="space-y-2">
         <input
-          type="email"
+          type="email" 
           bind:value={email}
           placeholder="Email"
           class="w-full rounded border p-2"
@@ -34,7 +35,7 @@
           placeholder="Password"
           class="w-full rounded border p-2"
           required
-        />
+        /> 
       </div>
       <button type="submit" class="mt-3 w-full rounded bg-emerald-500 p-2 text-white hover:bg-emerald-600">
         Log in
@@ -50,13 +51,16 @@
     
     </form>
 
-
     <div class="space-y-3 flex flex-col items-center ">
       {#each loginMethods as loginMethod}
         <button type="submit" class="w-full rounded-full bg-white p-2 text-gray-700 border shadow-sm">
-          Sign in with {loginMethod}
+          Sign in with {loginMethod} 
         </button>
-      {/each}
+      {/each} 
+    </div>
+ 
+    <div class="flex justify-end">
+      <span class="text-blue-500 mt-2 hover:text-blue-700">Don't have an account?</span>
     </div>
   </div>
 
