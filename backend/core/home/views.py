8ab13409c8ba0eab3cfb5aa54f7dc9618bec7770
HoweZae <jdtomanan@up.edu.ph>
2025-02-10 16:38:34 +0800

    docs: Spacing

--- backend/core/home/views.py
+++ backend/core/home/views.py
@@ -8,16 +8,20 @@ from home.serializer import UserSerializer
 @api_view(["POST", "GET"])
 def index(request):
     users = {"john123@gmail.com": "password"}
+    
     if request.method == "GET":
         return Response({"message": "Welcome to the login page"})
+    
     data = request.data
     print("HELLO")
+
     if data:
         if data["email"] in users:
             if data["password"] == users[data["email"]]:
                 return Response({"message": "Login successful"})
             else:
                 return Response({"message": "Invalid password"})
+            
         else:
             return Response({"message": "Invalid username"})
 
@@ -29,6 +33,7 @@ def person(request):
         persons = User.objects.all()
         serializer = UserSerializer(persons, many=True)
         return Response(serializer.data)
+    
     if request.method == "POST":
         data = request.data
         serializer = UserSerializer(data=data)
