654e98f15abeba57441a8199986be4632236ddbb
HoweZae <jdtomanan@up.edu.ph>
2025-02-10 19:42:47 +0800

    feat(Tests): Add to-do console tests for user, recipe tables

---
+++ tests/console-db-tests.spec.js
@@ -0,0 +1,27 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+
+test('User table', ()=>{
+    test('should reflect added user entry in database', ()=>{
+        // TODO
+    });
+
+    test('should reflect changes in user entry after edit', ()=>{
+        // TODO
+    });
+});
+
+test('Recipes table', ()=>{
+    test('should reflect added recipe entry in database', ()=>{
+        // TODO
+    });
+
+    test('should reflect changes in recipe entry after edit', ()=>{
+        // TODO
+    });
+
+    test('should properly add user comment inside recipe entry', ()=>{
+        // TODO
+    });
+});
