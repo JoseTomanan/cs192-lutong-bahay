bd2330c75008b1e1ee5b200a42ad9a8834231689
SleepyFeeshy <86706526+SleepyFeeshy@users.noreply.github.com>
2025-02-10 16:19:36 +0800

    Set Django settings.py to use PostgreSQL

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "lutongBahayDatabase",
+        "USER": "postgres",
+        "PASSWORD": "11789",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
