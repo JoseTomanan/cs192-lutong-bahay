cadeeed1777419f2586734ceea878a9e31c629a7
HoweZae <jdtomanan@up.edu.ph>
2025-02-10 16:40:05 +0800

    feat(Tests): Initialize tests for user table

---
+++ tests/user-table.spec.js
@@ -0,0 +1,14 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('Adding user', ()=>{
+    
+});
+
+test('Suspending a user', ()=>{
+    
+});
+
+test('Logging in with user', ()=>{
+    
+});
