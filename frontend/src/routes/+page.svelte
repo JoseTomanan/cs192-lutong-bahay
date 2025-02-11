8988b1c872ea20c9fa04cbf89ba6162474c336d6
HoweZae <jdtomanan@up.edu.ph>
2025-02-11 20:19:59 +0800

    fix(Frontend): Readability

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,13 +1,17 @@
 <script>
-  let pages = ['dashboard', 'login', 'search']
+    let pages = ['dashboard', 'login', 'search']
 </script>
 
 
 <div class="p-4 flex flex-col space-y-3">
-  <h1 class="text-2xl font-bold text-gray-800">links</h1>
-  <nav class="flex flex-col space-y-2">
-    {#each pages as page}
-      <a href="/{page}" class="text-blue-500 hover:text-blue-700">{page}</a>
-    {/each}  
-  </nav>
+    <h1 class="text-2xl font-bold text-gray-800">
+        links
+    </h1>
+    <nav class="flex flex-col space-y-2">
+        {#each pages as page}
+            <a href="/{page}" class="text-blue-500 hover:text-blue-700">
+                {page}
+            </a>
+        {/each}  
+    </nav>
 </div> 
