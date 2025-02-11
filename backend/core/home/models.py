45a4e306981b35de0aef15c9070327690cea0a3b
SleepyFeeshy <86706526+SleepyFeeshy@users.noreply.github.com>
2025-02-11 14:20:32 +0800

    Added recipe model to backend models.py

--- backend/core/home/models.py
+++ backend/core/home/models.py
@@ -7,3 +7,6 @@ class User(models.Model):
     password = models.CharField(max_length=100)
 
     objects = models.Manager()
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
