1627d6288e8ba947d34b98527e780b6cb494c21f
SleepyFeeshy <86706526+SleepyFeeshy@users.noreply.github.com>
2025-02-13 07:38:59 +0800

    HTML Table width dbtest smaller

--- frontend/src/routes/dbtest/+page.svelte
+++ frontend/src/routes/dbtest/+page.svelte
@@ -1,111 +1,120 @@
 <script>
-	import { goto } from "$app/navigation";
-  import UserRow from "./UserRow.svelte";
+	import { goto } from '$app/navigation';
+	import UserRow from './UserRow.svelte';
 
-  let users = [];
-  let inputEmail = "";
-  let inputPassword = "";
-  let error="";
+	let users = [];
+	let inputEmail = '';
+	let inputPassword = '';
+	let error = '';
 
-  async function fetchUsers() {
-    const response = await fetch("http://127.0.0.1:8000/users/");
-    users = await response.json();
-  }
+	async function fetchUsers() {
+		const response = await fetch('http://127.0.0.1:8000/users/');
+		users = await response.json();
+	}
 
-  fetchUsers()
+	fetchUsers();
 
-  async function submitUserForm() {
-    console.log(inputEmail, inputPassword)
-      const response = await fetch('http://127.0.0.1:8000/add-user/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email: inputEmail, password: inputPassword })
-      });
+	async function submitUserForm() {
+		console.log(inputEmail, inputPassword);
+		const response = await fetch('http://127.0.0.1:8000/add-user/', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ email: inputEmail, password: inputPassword })
+		});
 
-      const data = await response.json();
-      console.log(data)
-      
-      if (response.ok) {
-        console.log("User add successful");
-        return;
-      } else
-      {
-        console.log("User add fail");
-      }
-  }
+		const data = await response.json();
+		console.log(data);
 
-  async function functionDeleteUserForm(input_id) {
-    console.log(input_id)
-      const response = await fetch('http://127.0.0.1:8000/delete-user/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({id: input_id})
-      });
+		if (response.ok) {
+			console.log('User add successful');
+			return;
+		} else {
+			console.log('User add fail');
+		}
+	}
 
-      const data = await response.json();
-      console.log(data)
-      
-      if (response.ok) {
-        console.log("User delete successful");
-        goto('dbtest')
-        return;
-      } else
-      {
-        console.log("User add fail");
-      }
-  }
+	async function functionDeleteUserForm(input_id) {
+		console.log(input_id);
+		const response = await fetch('http://127.0.0.1:8000/delete-user/', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ id: input_id })
+		});
+
+		const data = await response.json();
+		console.log(data);
+
+		if (response.ok) {
+			console.log('User delete successful');
+			goto('dbtest');
+			return;
+		} else {
+			console.log('User add fail');
+		}
+	}
 </script>
 
-
-<div class="flex flex-col">
-  <h1 class="text-2xl font-bold text-gray-800">User Database</h1>
-  <div class="-m-1.5 overflow-x-auto">
-    <div class="p-1.5 min-w-full inline-block align-middle">
-      <div class="overflow-hidden">
-        <table class="min-w-full divide-y divide-gray-200">
-          <thead>
-            <tr>
-              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">ID</th>
-              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Email</th>
-              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Password</th>
-              <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Action</th>
-            </tr>
-          </thead>
-          <tbody class="divide-y divide-gray-200">
-            {#each users as user}
-              <UserRow userProp={user} functionProp={functionDeleteUserForm} />
-            {/each}  
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800"><form id="form1">x</form></td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
-                <input 
-                  form="form1" type="text" name="email"
-                  bind:value={inputEmail}
-                />
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
-                <input form="form1" type="text" name="password"
-                bind:value={inputPassword}
-                />
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
-                <button 
-                  form="form1"
-                  type="submit" 
-                  class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none"
-                  on:click={submitUserForm}
-                >
-                Add
-              </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
+<div class="flex">
+	<div class="w-lg">
+		<h1 class="text-2xl font-bold text-gray-800">User Database</h1>
+		<div class="-m-1.5 overflow-x-auto">
+			<div class="inline-block min-w-full p-1.5 align-middle">
+				<div class="overflow-hidden">
+					<table class="min-w-full divide-y divide-gray-200">
+						<thead>
+							<tr>
+								<th
+									scope="col"
+									class="px-6 py-3 text-start text-xs font-medium uppercase text-gray-500">ID</th
+								>
+								<th
+									scope="col"
+									class="px-6 py-3 text-start text-xs font-medium uppercase text-gray-500">Email</th
+								>
+								<th
+									scope="col"
+									class="px-6 py-3 text-start text-xs font-medium uppercase text-gray-500"
+									>Password</th
+								>
+								<th
+									scope="col"
+									class="px-6 py-3 text-end text-xs font-medium uppercase text-gray-500">Action</th
+								>
+							</tr>
+						</thead>
+						<tbody class="divide-y divide-gray-200">
+							{#each users as user}
+								<UserRow userProp={user} functionProp={functionDeleteUserForm} />
+							{/each}
+							<tr>
+								<td class="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-800"
+									><form id="form1">x</form></td
+								>
+								<td class="whitespace-nowrap px-6 py-4 text-sm text-gray-800">
+									<input form="form1" type="text" name="email" bind:value={inputEmail} />
+								</td>
+								<td class="whitespace-nowrap px-6 py-4 text-sm text-gray-800">
+									<input form="form1" type="text" name="password" bind:value={inputPassword} />
+								</td>
+								<td class="whitespace-nowrap px-6 py-4 text-end text-sm font-medium">
+									<button
+										form="form1"
+										type="submit"
+										class="inline-flex items-center gap-x-2 rounded-lg border border-transparent text-sm font-semibold text-blue-600 hover:text-blue-800 focus:text-blue-800 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+										on:click={submitUserForm}
+									>
+										Add
+									</button>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
