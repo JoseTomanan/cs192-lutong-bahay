79afbf9b64cd12f82b4d605feafd077efa68c961
HoweZae <jdtomanan@up.edu.ph>
2025-02-18 12:49:33 +0800

    feat: Replace login page to make mergeable

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,111 +1,128 @@
-<script lang="ts">
+<script>
 	import { goto } from "$app/navigation";
-  // import { isAuthenticated, setAuth } from '$lib/stores/auth';
-  
-  let identifier = '';
-  let password = '';
-  let error = '';
-  
-  let loginMethods = [
-      {name: 'Google', icon: '/google.webp'}, 
-      {name: 'Facebook', icon: '/facebook.png'}, 
-  ] 
 
-  async function handleSubmit() {
-    try { 
-      const response = await fetch('http://localhost:8000/api/login/', {
-        method: 'POST', 
+  let users = [];
+  let inputEmail = "";
+  let inputPassword = "";
+  let error="";
+
+  async function fetchUsers() {
+    const response = await fetch("http://127.0.0.1:8000/users/");
+    users = await response.json();
+  }
+
+  fetchUsers()
+
+  async function submitUserForm() {
+    console.log(inputEmail, inputPassword)
+      const response = await fetch('http://127.0.0.1:8000/add-user/', {
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ identifier, password })
+        body: JSON.stringify({ email: inputEmail, password: inputPassword })
       });
 
       const data = await response.json();
-      const success = data.success 
-      const message = data.message
-
-      // console.log(login status:${message}) // don't remove; for testing
-
-      if (!success) {
-        alert(message)
-      } else {
-        alert(message) // Hamdi's addition for testing
-        // setAuth(true)
-        goto('/home') 
-      }
+      console.log(data)
       
-      if (!response.ok) { 
-        const data = await response.json();
-        const error = data.error;
+      if (response.ok) {
+        console.log("User add successful");
         return;
+      } else
+      {
+        console.log("User add fail");
       }
-      
-    } catch (err) {
-      console.log('Login failed')
-    }
+  }
 
-  } 
+  async function functionDeleteUserForm(input_id) {
+    console.log(input_id)
+      const response = await fetch('http://127.0.0.1:8000/delete-user/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({id: input_id})
+      });
+
+      const data = await response.json();
+      console.log(data)
+      
+      if (response.ok) { 
+        console.log("User delete successful");
+        goto('dbtest')
+        return;
+      } else
+      {
+        console.log("User add fail");
+      }
+  }
 </script>
 
-<section>
-  <div
-      class="bg-background w-full flex flex-col h-5/6 justify-center max-w-xl space-y-3 rounded-lg px-12 py-12 shadow-xl border"
-      style="background: url(/liempo_edited.xpng); background-size:cover"
-  >
-      <form on:submit|preventDefault={handleSubmit} class="space-y-4">
-          <h1 class="text-4xl font-bold -mb-4 text-main">
-              Lutong Bahay
-          </h1>
-          <h2 class="text-dark_gray pb-7 text-lg italic">
-              Your database for Filipino recipes
-          </h2>
-          
-          <div class="space-y-2"> 
-              <input
-                  type="text"
-                  bind:value={identifier}
-                  placeholder="Username of email"
-                  class="space-y-2 w-full rounded border p-2 focus:outline-secondary focus:ring-0"
-                  required
-              />
-              
-              <input
-                  type="password"
-                  bind:value={password}
-                  placeholder="Password" 
-                  class="space-y-2 w-full rounded border p-2 focus:outline-secondary focus:ring-0"
-                  required
-              />
-          </div>
 
-          <button type="submit" class="w-full rounded bg-main p-2 text-white hover:bg-main_dark">
-              Log in
-          </button>
-          
-          <div class="flex items-center justify-center pt-3 pb-2">
-              <hr class="flex-grow border-gray-200">
-              <span class="px-4 text-dark_gray">OR</span>
-              <hr class="flex-grow border-gray-200">
-          </div>
-      </form>
-
-      <div class="space-y-3 flex flex-col items-center ">
-          {#each loginMethods as {name, icon}} 
-              <button
-                  type="submit"
-                  class="w-full rounded-full bg-white p-2 text-gray-700 border hover:bg-gray-100 flex items-center gap-3 justify-center"
-              >
-                  <img src={icon} alt={name} class="w-6 h-6">
-                  Sign in with {name} 
-              </button>  
-          {/each}   
-      </div> 
-
-      <div class="flex justify-end"> 
-          <a class="mt-2 text-secondary hover:text-secondary_dark hover:underline text-opacity-90" href="#top">
-              Don't have an account?
-          </a>
+<div class="flex flex-col">
+  <h1 class="text-2xl font-bold text-gray-800">User Database</h1>
+  <div class="-m-1.5 overflow-x-auto">
+    <div class="p-1.5 min-w-full inline-block align-middle">
+      <div class="overflow-hidden">
+        <table class="min-w-full divide-y divide-gray-200">
+          <thead>
+            <tr>
+              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">ID</th>
+              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Email</th>
+              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Password</th>
+              <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Action</th>
+            </tr>
+          </thead>
+          <tbody class="divide-y divide-gray-200">
+            {#each users as user}
+            <tr>
+              <td 
+                class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">{user.id}
+              </td>
+              <td 
+                class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{user.email}
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{user.password}
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                <button 
+                  type="submit" 
+                  class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none"
+                  on:click={() => functionDeleteUserForm(user.id)}
+                  >
+                  Delete
+                </button>
+              </td>
+            </tr>
+            {/each}  
+            <tr>
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800"><form id="form1">x</form></td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                <input 
+                  form="form1" type="text" name="email"
+                  bind:value={inputEmail}
+                />
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                <input form="form1" type="text" name="password"
+                bind:value={inputPassword}
+                />
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                <button 
+                  form="form1"
+                  type="submit" 
+                  class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none"
+                  on:click={submitUserForm}
+                >
+                Add
+              </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
+    </div>
   </div>
-</section> 
+</div>
