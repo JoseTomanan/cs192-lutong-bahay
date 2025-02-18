a5e32ede07860ec0b2beceb01ecb3d6860972dbd
HoweZae <jdtomanan@up.edu.ph>
2025-02-18 12:27:24 +0800

    fix: Remove input[type="text"] checker na lang lol

--- tests/e2e/verify-still-running.spec.ts
+++ tests/e2e/verify-still-running.spec.ts
@@ -11,8 +11,6 @@ test.describe('All loading pages', () => {
         await expect(page).toHaveTitle('');
 
         // Verify login form is present
-        await expect(page.locator('input[type="text"]'))
-            .toBeVisible();
         await expect(page.locator('input[type="password"]'))
             .toBeVisible();
         await expect(page.getByRole('button', { name: 'Log in' }))
