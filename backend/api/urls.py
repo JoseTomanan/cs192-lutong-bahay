08469844d7828d773cf68d6b4468aef23a5cf6cd
SleepyFeeshy <86706526+SleepyFeeshy@users.noreply.github.com>
2025-02-18 15:44:54 +0800

    fix: Transferred urls from core/urls to api/urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from home.views import login, person
+
+from home.views import get_users, add_user, delete_user
+from recipes.views import get_recipes
+
 
 urlpatterns = [
     path("login/", login),
     path("person/", person),
+
+    path("users/", get_users),
+    path("add-user/", add_user, name="add-user"),
+    path("delete-user/", delete_user, name="delete-user"),
+    path("recipes/", include("recipes.urls")),
 ]
