1008cbe4d4f89f515e403c0af75399205e2d3e2e
HoweZae <jdtomanan@up.edu.ph>
2025-02-19 15:56:11 +0800

    fix: Remove obsolete import, url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
-from home.views import login, person
+from home.views import login
 
 from home.views import get_users, add_user, delete_user
 
 urlpatterns = [
     path("login/", login),
-    path("person/", person),
-
     path("users/", get_users),
     path("add-user/", add_user, name="add-user"),
     path("delete-user/", delete_user, name="delete-user"),
