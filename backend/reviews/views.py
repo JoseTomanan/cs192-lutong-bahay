9e4b45b0765ea398037c0f53f0a2d33c20325fcf
Zachary Michael Romualdez <zromualdez@gmail.com>
2025-02-20 17:28:55 +0800

    feat(Backend): Created function in reviews views to add review, created corresponding urls

--- backend/reviews/views.py
+++ backend/reviews/views.py
@@ -1,3 +1,19 @@
+from rest_framework.decorators import api_view
 from django.shortcuts import render
+from rest_framework.response import Response
+
+from reviews.models import Review
+from reviews.serializer import ReviewSerializer
 
 # Create your views here.
+# Create your views here.
+@api_view(['POST'])
+def post_review(request):
+    recipeId = request.data['recipeId']
+    reviewerId = request.data['reviewerId']
+    reviewRating = request.data['reviewRating']
+    reviewString = request.data['reviiewString']
+    review = Review.create(recipeId=recipeId, reviewerId=reviewerId, reviewText=reviewRating, reviewString=reviewString)
+    review.save()
+    return Response({"message": "Review posted successfully"})
+    
