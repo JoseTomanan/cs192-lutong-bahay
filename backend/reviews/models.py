248bae33e2bf577b3fb4ab0b546a38773d69ce42
Zachary Michael Romualdez <zromualdez@gmail.com>
2025-02-20 17:13:52 +0800

    feat(Database): Migrated Recipe model

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    recipeId = models.IntegerField()
+    reviewerId = models.IntegerField()
+    rating = models.IntegerField()
+    reviewString = models.CharField(max_length=200)
+    
+    
