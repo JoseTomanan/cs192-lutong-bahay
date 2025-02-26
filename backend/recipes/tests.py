03f32c019376eb66a1c526ed36b6a39fbd68e55e
HoweZae <jdtomanan@up.edu.ph>
2025-02-26 15:40:39 +0800

    chore: Minor

--- backend/recipes/tests.py
+++ backend/recipes/tests.py
@@ -1,17 +1,18 @@
+import json
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
 from unittest.mock import patch
 from .models import Recipe
-
-MOCK_DATA = {}
+from .serializer import RecipeSerializer
 
 class TestRecipes(APITestCase):
     def setup(self):
-        for item in MOCK_DATA:
-            Recipe.objects.create()
+        Recipe.objects.create(
+            
+        )
 
-    # @patch(Recipe.views.check_connection_status)
+    @patch("Recipe.views.check_connection_status")
     def test_no_database_connection(self, mock_status):
         ...
 
