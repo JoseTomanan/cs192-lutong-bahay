164ad3e858b920e87111f9e00f5ca9cf8016ea88
HoweZae <jdtomanan@up.edu.ph>
2025-02-26 16:08:55 +0800

    docs: Spacing

--- backend/recipes/tests.py
+++ backend/recipes/tests.py
@@ -3,14 +3,39 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
 from unittest.mock import patch
+
 from .models import Recipe
 from .serializer import RecipeSerializer
 
 class TestRecipes(APITestCase):
     def setup(self):
-        Recipe.objects.create(id=1,recipeName="Hotdog",cookDifficulty="1",servings=1,equipment="",instructions="- slice hotdog\n- fry hotdog on pan until cooked",ingredients=[])
-        Recipe.objects.create(id=2,recipeName="Fried egg",cookDifficulty="1",servings=1,equipment="",instructions="- crack egg\n- fry egg on pan until cooked",ingredients=[])
-        Recipe.objects.create(id=3,recipeName="Tortang talong",cookDifficulty="1",servings=1,equipment="",instructions="- crack egg on eggplant\n- fry on pan until cooked",ingredients=[])
+        Recipe.objects.create(
+            id=1,
+            recipeName="Hotdog",
+            cookDifficulty="1",
+            servings=1,
+            equipment="",
+            instructions="- slice hotdog\n- fry hotdog on pan until cooked",
+            ingredients=[]
+            )
+        Recipe.objects.create(
+            id=2,
+            recipeName="Fried egg",
+            cookDifficulty="1",
+            servings=1,
+            equipment="",
+            instructions="- crack egg\n- fry egg on pan until cooked",
+            ingredients=[]
+            )
+        Recipe.objects.create(
+            id=3,
+            recipeName="Tortang talong",
+            cookDifficulty="1",
+            servings=1,
+            equipment="",
+            instructions="- crack egg on eggplant\n- fry on pan until cooked",
+            ingredients=[]
+            )
 
     @patch("Recipe.views.check_connection_status")
     def test_no_database_connection(self, mock_status):
