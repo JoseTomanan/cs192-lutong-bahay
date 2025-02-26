06e0b6117a54471a6ec77cbf25fc4d9d3153929c
Hamdisky <mstuan@up.edu.ph>
2025-02-26 16:05:22 +0800

    chore: filled setup method of testRecipes

--- backend/recipes/tests.py
+++ backend/recipes/tests.py
@@ -8,9 +8,9 @@ from .serializer import RecipeSerializer
 
 class TestRecipes(APITestCase):
     def setup(self):
-        Recipe.objects.create(
-            
-        )
+        Recipe.objects.create(id=1,recipeName="Hotdog",cookDifficulty="1",servings=1,equipment="",instructions="- slice hotdog\n- fry hotdog on pan until cooked",ingredients=[])
+        Recipe.objects.create(id=2,recipeName="Fried egg",cookDifficulty="1",servings=1,equipment="",instructions="- crack egg\n- fry egg on pan until cooked",ingredients=[])
+        Recipe.objects.create(id=3,recipeName="Tortang talong",cookDifficulty="1",servings=1,equipment="",instructions="- crack egg on eggplant\n- fry on pan until cooked",ingredients=[])
 
     @patch("Recipe.views.check_connection_status")
     def test_no_database_connection(self, mock_status):
