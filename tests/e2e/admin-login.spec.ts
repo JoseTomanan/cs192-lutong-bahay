a415dbd777c43874cca0eb5d88a52043b44f45ac
HoweZae <jdtomanan@up.edu.ph>
2025-03-12 19:58:30 +0800

    feat: Admin login tests

--- tests/e2e/admin-login.spec.ts
+++ tests/e2e/admin-login.spec.ts
@@ -1,5 +1,37 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Admininstrator login', () => {
+	test('should show admin credentials are incorrect', async ({ page }) => {
+		await page.goto('/login');
+    
+    await page.fill('input[type="text"]', 'admin');
+    await page.fill('input[type="password"]', 'wrongpassword');
 
+    page.on('dialog', async (dialog) => {
+      console.log('Dialog message:', dialog.message());
+      expect(dialog.message()).toBe('Invalid credentials');
+      await dialog.dismiss();
+    });
+
+    await page.click('button[type="submit"]');
+    
+    await page.waitForURL('/login');
+	});
+
+	test('should lead to admin pagevalid credentials are entered', async ({ page }) => {
+    await page.goto('/login');
+
+    await page.fill('input[type="text"]', 'admin');
+    await page.fill('input[type="password"]', 'adminadmin');
+
+    page.on('dialog', async (dialog) => {
+      console.log('Dialog message:', dialog.message());
+      expect(dialog.message()).toBe('Login successful');
+      await dialog.dismiss();
+    });
+    
+    await page.click('button[type="submit"]');
+  
+    await page.waitForURL('/admin');
+  });
 });
