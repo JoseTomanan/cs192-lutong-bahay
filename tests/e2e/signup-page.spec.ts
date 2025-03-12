8da990a4475b6bef19e2576c0c0128fdd06c5eb4
HoweZae <jdtomanan@up.edu.ph>
2025-03-12 20:10:34 +0800

    chore: init signup page

--- tests/e2e/signup-page.spec.ts
+++ tests/e2e/signup-page.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Signup page', () => {
+	test('should not be able to log in with ', async ({ page }) => { 
+		await page.goto('/register')
 
+		
+	});
 });
