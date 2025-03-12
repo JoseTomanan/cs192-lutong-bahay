a01b7d73a593ffea16536cf1c583ec435282cf58
HoweZae <jdtomanan@up.edu.ph>
2025-03-12 20:23:49 +0800

    feat: Finish signup page

--- tests/e2e/signup-page.spec.ts
+++ tests/e2e/signup-page.spec.ts
@@ -1,9 +1,44 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Signup page', () => {
-	test('should not be able to log in with ', async ({ page }) => { 
-		await page.goto('/register')
+test.beforeEach(async ({ page }) => {
+  await page.goto('/register')
+});
 
-		
-	});
+test.describe('Signup page', () => {
+  test('should not allow invalid username', async ({ page }) => { 
+    await page.fill('input[type="text"]', 'testuser');
+    await page.fill('input[placeholder="Password"]', 'doesnotmatter');
+    await page.fill('input[placeholder="Confirm password"]', 'doesnotmatter');
+
+    page.on('dialog', async (dialog) => {
+      console.log('Dialog message:', dialog.message());
+      expect(dialog.message()).toBe('No database connection');
+      await dialog.dismiss();
+    });
+
+    await page.click('button[type="submit"]');
+    await page.waitForURL('/register');
+  });
+
+  test('should not allow invalid password', async ({ page }) => { 
+    await page.fill('input[type="text"]', 'testnewuser');
+    await page.fill('input[placeholder="Password"]', 'origpass');
+    await page.fill('input[placeholder="Confirm password"]', 'doesnotmatch');
+
+    page.on('dialog', async (dialog) => {
+      console.log('Dialog message:', dialog.message());
+      expect(dialog.message()).toBe('No database connection');
+      await dialog.dismiss();
+    });
+
+    await page.click('button[type="submit"]');
+    await page.waitForURL('/register');
+  });
+
+  test('should recognize when new user is created', async ({ page }) => { 
+    /**
+     * This part cannot be automated
+     * Because of repeating credentials leading to already created users
+     */
+  });
 });
