c57e1e7aea70190dce58564016de5a597dadf852
Zachary Michael Romualdez <zromualdez@gmail.com>
2025-03-23 19:24:47 +0800

    feat(Database): Migrations for new RecipeIngredients table

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -9,12 +9,6 @@ class Ingredients(models.Model):
     proteinPerUnit = models.IntegerField()
     fatPerUnit = models.IntegerField()
     carbsPerUnit = models.IntegerField()
-
-class RecipeIngredients(models.Model):
-    ingredientId = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    unit = models.CharField(max_length=50)
-
 class UserIngredientsInventory(models.Model):
     ingredientId = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
@@ -25,12 +19,14 @@ class Recipe(models.Model):
     price = models.FloatField()
     cookDifficulty = models.CharField(max_length=100)
     servings = models.IntegerField()
-    ingredients = models.ForeignKey(RecipeIngredients, on_delete=models.CASCADE)
     equipment = models.CharField(max_length=1000)
     instructions = models.CharField(max_length=1000)
     ratings = models.FloatField()
-
-
+class RecipeIngredients(models.Model):
+    ingredientId = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    unit = models.CharField(max_length=50)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 class CookedBy(models.Model):
     recipe = models.ForeignKey(
         Recipe, related_name="has_ingredients", on_delete=models.CASCADE
