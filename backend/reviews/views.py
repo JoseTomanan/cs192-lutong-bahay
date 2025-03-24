121b9e3123bbd2e75f2c86d8e947cfe62e7525d2
SleepyFeeshy <86706526+SleepyFeeshy@users.noreply.github.com>
2025-03-24 15:07:15 +0800

    fix(Backend): Update to post_review endpoint, get objects from id

--- backend/reviews/views.py
+++ backend/reviews/views.py
@@ -4,16 +4,26 @@ from rest_framework.response import Response
 
 from reviews.models import Review
 from reviews.serializer import ReviewSerializer
+from recipes.models import Recipe
+
+from django.contrib.auth.models import User
 
 # Create your views here.
 # Create your views here.
 @api_view(['POST'])
 def post_review(request):
-    recipe = request.data['recipe']
-    reviewer = request.data['reviewer']
+    recipeId = request.data['recipeId']
+
+    # get Recipe object from ID
+    recipeObject = Recipe.objects.get(pk=recipeId)
+
+    # get Reviewer from ID
+    
+    reviewerId = request.data['reviewerId']
+    reviewerObject = User.objects.get(pk=reviewerId)
     reviewRating = request.data['reviewRating']
     reviewString = request.data['reviewString']
-    review = Review.create(recipe=recipe, reviewer=reviewer, reviewRating=reviewRating, reviewString=reviewString)
-    # review.save()
+    review = Review.create(recipe=recipeObject, reviewer=reviewerObject, reviewRating=reviewRating, reviewString=reviewString)
+    review.save()
     return Response({"message": "Review posted successfully"})
     
