23dcc835208222e6dca5b6a517ebc23cf5e6df89
Zachary Michael Romualdez <zromualdez@gmail.com>
2025-03-25 09:28:07 +0800

    fix(Frontend): minor frontend changes

--- frontend/src/lib/components/RecipeReview.svelte
+++ frontend/src/lib/components/RecipeReview.svelte
@@ -32,5 +32,6 @@
 
 <div class="container my-1 py-3 px-10">
 	<p class="font-bold">{username}</p>
-	<p>{props.recipeReview.reviewString}</p>
+	<p class="my-1">{props.recipeReview.reviewString}</p>
+	<p class="text-sm text-zinc-400">Rating: {props.recipeReview.reviewRating}</p>
 </div>
--- frontend/src/routes/recipes/[slug]/+page.svelte
+++ frontend/src/routes/recipes/[slug]/+page.svelte
@@ -56,25 +56,6 @@
 		}
   }
 
-  async function functionUserById() {
-		const response = await fetch('http://127.0.0.1:8000/api/users/fetch-user-by-id/', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ id: user_id })
-		});
-
-		let userObject = await response.json();
-
-		if (response.ok) {
-			console.log('User fetch successful');
-			return;
-		} else {
-			console.log('User fetch fail');
-		}
-  }
-
   async function postReview() {
 	const response = await fetch('http://127.0.0.1:8000/api/reviews/post-review/', {
 			method: 'POST',
