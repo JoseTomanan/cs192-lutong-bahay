2baf04da09589b3bc16284d4c41bd8ff5efb4258
Elijah Mejilla <111504836+jgmejilla@users.noreply.github.com>
2025-03-26 18:08:44 +0800

    fix(Frontend): Minor layout changes

--- frontend/src/routes/submit_recipe/+page.svelte
+++ frontend/src/routes/submit_recipe/+page.svelte
@@ -114,8 +114,8 @@ async function handleAddRecipe() {
 
 </script>
 
-<h1 class="my-2 text-2xl font-sans text-main font-bold">Add a Recipe</h1>
-<form on:submit|preventDefault={handleAddRecipe} class="space-y-4">
+<TitleText text="Add a recipe" />
+<form on:submit|preventDefault={handleAddRecipe} class="space-y-4 mt-4">
     <input 
         type="text" 
         class="block w-1/3 p-4 text-sm space-y-2 rounded border focs:border-none focus:outline-gray-700 focus:ring-0"
