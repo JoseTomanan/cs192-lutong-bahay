48cedd29ee2214f04816b8be1c5bc44a7708c005
HoweZae <jdtomanan@up.edu.ph>
2025-03-26 21:30:37 +0800

    feat: Visual adjustment

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -64,40 +64,41 @@ pt-8 px-2">
           {#each admin_pages as route}
             <a
               href="/{route[0]}" 
-              class="px-5 py-3 rounded-lg italic font-semibold {
+              class="px-5 py-3 rounded-lg {
                 page.url.pathname === '/' + route[0]
                 ? 'bg-main text-white' 
-                : 'text-gray-700 bg-white hover:bg-gray-100'
+                : 'text-gray-700 bg-main bg-opacity-10 hover:bg-opacity-20'
               }"
             >
               { route[1] }
             </a>
           {/each}
-
-          <a 
-            target="_blank"
-            href="http://127.0.0.1:8000/admin" 
-            class="px-5 py-3 rounded-lg text-gray-700 bg-white hover:bg-gray-100 flex justify-start gap-2"
-          >
-            <span class="italic font-semibold">Open in Django</span>
-            <img src="{OutgoingLink}" alt="" class="w-4 mt-0.5 opacity-70">
-          </a>
         {/if}
     </div>
   </div>
 
   
-    {#if ($usernameStore) != ""}
-      <p class="pl-2 text-gray-400">@{$usernameStore}</p>
-    {/if}
+  {#if ($usernameStore) != ""}
+    <p class="pl-2 text-gray-400">@{$usernameStore}</p>
+  {/if}
   <div class="p-2">
-      <button 
-          on:click={logout}
-          class="w-full px-4 py-3 rounded-lg bg-gray-800 text-white
-             hover:bg-gray-700 "
+    {#if $isAdmin}
+      <a 
+        target="_blank"
+        href="http://127.0.0.1:8000/admin" 
+        class="px-5 py-3 my-2 rounded-lg text-gray-500 bg-white hover:bg-gray-100 flex justify-start gap-2"
       >
-          Logout
-      </button>
+        <span class="italic">Open in Django</span>
+        <img src="{OutgoingLink}" alt="" class="w-4 mt-0.5 opacity-60"/>
+      </a>
+    {/if}
+    <button 
+      on:click={logout}
+      class="w-full px-4 py-3 rounded-lg bg-gray-800 text-white
+          hover:bg-gray-700 "
+    >
+        Logout
+    </button>
   </div>
 </nav>
 
---
+++ frontend/src/routes/manage_review/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import TitleText from "$lib/components/TitleText.svelte";
+
+  let username = "testuser"
+</script>
+
+<TitleText text={`Manage reviews`}/>
+
+<form class="">
+  <!-- search bar -->
+
+  <!-- all same components as submit recipes -->
+</form>
