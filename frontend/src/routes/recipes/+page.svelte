10c4b4d5c1359dea6b4fd84e052e17a0a8867274
HoweZae <jdtomanan@up.edu.ph>
2025-03-28 14:41:16 +0800

    docs: Indentation

--- frontend/src/routes/recipes/+page.svelte
+++ frontend/src/routes/recipes/+page.svelte
@@ -1,10 +1,3 @@
-<style> 
-  * {
-    font-family: "Inter" 
-  }
-</style> 
-
-
 <script lang="ts">
   import { onMount } from 'svelte'
   import RecipeCard from '$lib/components/RecipeCard.svelte'
@@ -119,69 +112,91 @@
 <!-- FLOWBITE https://flowbite.com/docs/forms/search-input/ -->  
 
 <div class="flex gap-2">
-<div class="space-y-5 w-1/3">
-  <form on:submit|preventDefault={fetchRecipes} class="max-w-md"> 
-    <label for="search" class="mb-2 text-sm font-medium text-black sr-only">Search</label>
-    <div class="relative">
+  <div class="space-y-5 w-1/3">
+    <form on:submit|preventDefault={fetchRecipes} class="max-w-md"> 
+      <label
+        for="search"
+        class="mb-2 text-sm font-medium text-black sr-only"
+      >Search</label>
+      <div class="relative">
         <div class="absolute inset-y-0 start-0 flex  ps-3 pointer-events-none">
-            <svg class="w-4 h-4 text-dark_gray" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-            </svg>
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+          <svg
+            class="w-4 h-4 text-dark_gray"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          ></svg>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
         </div>
         <input 
           type="search" 
           id="default-search" 
           class="block w-full p-4 text-sm space-y-2 rounded border focs:border-none focus:outline-gray-700 focus:ring-0"
           bind:value={recipeName}
-          placeholder="Search Recipes..." required />
-        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-main hover:bg-main_dark font-medium rounded-lg text-sm px-4 py-2">Search</button>
-    </div> 
-  </form> 
-</div>
+          placeholder="Search Recipes..." required
+        />
+        <button
+          type="submit"
+          class="text-white absolute end-2.5 bottom-2.5 bg-main hover:bg-main_dark font-medium rounded-lg text-sm px-4 py-2"
+        >Search</button>
+      </div> 
+    </form> 
+  </div>
 
-<div class="space-y-5 w-1/3"> 
-  <form on:submit|preventDefault={fetchByIngredients} class="max-w-md"> 
-    <label for="search" class="mb-2 text-sm font-medium text-black sr-only">Search</label>
-    <div class="relative">
+  <div class="space-y-5 w-1/3"> 
+    <form on:submit|preventDefault={fetchByIngredients} class="max-w-md"> 
+      <label for="search" class="mb-2 text-sm font-medium text-black sr-only">Search</label>
+      <div class="relative">
         <div class="absolute inset-y-0 start-0 flex  ps-3 pointer-events-none">
-            <svg class="w-4 h-4 text-dark_gray" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-            </svg>
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            <svg
+              class="w-4 h-4 text-dark_gray"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none" viewBox="0 0 20 20"
+            ></svg>
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
         </div>
         <input 
           type="search" 
           id="default-search" 
           class="block w-full p-4 text-sm space-y-2 rounded border focs:border-none focus:outline-gray-700 focus:ring-0"
           bind:value={ingredients}   
-          placeholder="Filter by ingredient..." required />
-        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-gray-400 hover:bg-gray-500 font-medium rounded-lg text-sm px-4 py-2">Filter</button>
-    </div> 
-  </form> 
-</div>
+          placeholder="Filter by ingredient..." required
+        />
+        <button
+          type="submit"
+          class="text-white absolute end-2.5 bottom-2.5 bg-gray-400 hover:bg-gray-500 font-medium rounded-lg text-sm px-4 py-2"
+        >Filter</button>
+      </div> 
+    </form> 
+  </div>
 
-<!-- FLOWBITE https://flowbite.com/docs/forms/select/ -->
-<select
-  bind:value={sort} 
-  on:change={() => sortRecipes()}
-  class="bg-white border border-gray-200 text-gray-700 text-sm rounded focus:ring-main focus:border-2 focus:border-gray-700 block w-3/12 p-2.5">
+  <!-- FLOWBITE https://flowbite.com/docs/forms/select/ -->
+  <select
+    bind:value={sort} 
+    on:change={() => sortRecipes()}
+    class="bg-white border border-gray-200 text-gray-700 text-sm rounded focus:ring-main focus:border-2 focus:border-gray-700 block w-3/12 p-2.5"
+  >
+    {#each ['id', 'recipeName', 'price', 'cookDifficulty', 'ratings'] as sortMethod}
+      <option value={sortMethod}>
+        sort by {sortMethod}
+      </option>
+    {/each} 
+  </select>
 
-  {#each ['id', 'recipeName', 'price', 'cookDifficulty', 'ratings'] as sortMethod}
-    <option value={sortMethod}>sort by {sortMethod}</option>
-  {/each} 
-</select>
-
-<select
-  bind:value={direction}  
-  on:change={() => sortRecipes()} 
-  class="bg-white border border-gray-200 text-gray-700 text-sm rounded focus:ring-main focus:border-2 focus:border-gray-700 block w-3/12 p-2.5">
-
-  {#each ['ascending', 'descending'] as sortMethod} 
-    <option value={sortMethod}>sort by {sortMethod}</option>
-  {/each} 
-</select> 
- 
-
-
+  <select
+    bind:value={direction}  
+    on:change={() => sortRecipes()} 
+    class="bg-white border border-gray-200 text-gray-700 text-sm rounded focus:ring-main focus:border-2 focus:border-gray-700 block w-3/12 p-2.5"
+  >
+    {#each ['ascending', 'descending'] as sortMethod} 
+      <option value={sortMethod}>
+        sort by {sortMethod}
+      </option>
+    {/each} 
+  </select> 
 </div>
 <!-- FLOWBITE https://flowbite.com/docs/components/tables/ -->  
 
