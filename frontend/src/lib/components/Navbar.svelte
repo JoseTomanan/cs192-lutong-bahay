c9ed869d32a23005d38c530b91edd9f873648d95
HoweZae <jdtomanan@up.edu.ph>
2025-03-31 22:26:00 +0800

    docs: Spacing

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -37,50 +37,49 @@
   </div>
 </nav> -->
 
-
-
 <nav class="fixed top-0 left-0 h-screen w-64 border-r bg-white flex flex-col z-50
 pt-8 px-2">
   <!-- <div class="p-6 border-b">
-      <h1 class="text-2xl font-semibold text-main">Lutong Bahay</h1>
+    <h1 class="text-2xl font-semibold text-main">Lutong Bahay</h1>
   </div> -->
-
   <div class="flex-1 p-2">
     <div class="flex flex-col space-y-2">
-        {#each pages as route}
-          <a 
-            href="/{route}" 
-            class="px-5 py-3 rounded-lg {
-                page.url.pathname === '/' + route 
-                ? 'bg-gradient-to-bl from-gray-600 to-gray-700 text-white' 
-                : 'text-gray-700 bg-white hover:bg-gray-100'
+      {#each pages as route}
+        <a 
+          href="/{route}" 
+          class="px-5 py-3 rounded-lg {
+              page.url.pathname === '/' + route 
+              ? 'bg-gradient-to-bl from-gray-600 to-gray-700 text-white' 
+              : 'text-gray-700 bg-white hover:bg-gray-100'
             }"
-          >
-              {route[0].toUpperCase() + route.slice(1)}
-          </a>
-        {/each}
+        >
+            {route[0].toUpperCase() + route.slice(1)}
+        </a>
+      {/each}
 
-        {#if $isAdmin}
-          {#each admin_pages as route}
-            <a
-              href="/{route[0]}" 
-              class="px-5 py-3 rounded-lg {
+      {#if $isAdmin}
+        {#each admin_pages as route}
+          <a
+            href="/{route[0]}" 
+            class="px-5 py-3 rounded-lg {
                 page.url.pathname === '/' + route[0]
                 ? 'bg-main text-white' 
                 : 'text-gray-700 bg-main bg-opacity-10 hover:bg-opacity-20'
               }"
-            >
-              { route[1] }
-            </a>
-          {/each}
-        {/if}
+          >
+            { route[1] }
+          </a>
+        {/each}
+      {/if}
     </div>
   </div>
 
-  
   {#if ($usernameStore) != ""}
-    <p class="pl-2 text-gray-400">@{$usernameStore}</p>
+    <p class="pl-2 text-gray-400">
+      @{$usernameStore}
+    </p>
   {/if}
+
   <div class="p-2">
     {#if $isAdmin}
       <a 
@@ -92,13 +91,11 @@ pt-8 px-2">
         <img src="{OutgoingLink}" alt="" class="w-4 mt-0.5 opacity-60"/>
       </a>
     {/if}
+
     <button 
       on:click={logout}
-      class="w-full px-4 py-3 rounded-lg bg-gray-800 text-white
-          hover:bg-gray-700 "
-    >
-        Logout
-    </button>
+      class="w-full px-4 py-3 rounded-lg bg-gray-800 text-white hover:bg-gray-700 "
+    >Logout</button>
   </div>
 </nav>
 
