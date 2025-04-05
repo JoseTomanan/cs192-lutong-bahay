6e637f710a401e78b931aed3237d835b0a58dbb3
HoweZae <jdtomanan@up.edu.ph>
2025-04-05 19:18:13 +0800

    feat: Fix classes used

--- frontend/src/routes/submit_recipe/+page.svelte
+++ frontend/src/routes/submit_recipe/+page.svelte
@@ -117,7 +117,7 @@
 		  ];
 		
     currentIngredient;
-		ingredientQuantity = 0;
+		ingredientQuantity = 1;
 		console.log(ingredients);
 	};
 
@@ -129,11 +129,11 @@
 
 	const addEquipment = () => {
 		equipment = [
-			...equipment,
-			{equipmentName, equipmentQuantity}
-		];
+        ...equipment,
+        {equipmentName, equipmentQuantity}
+      ];
 		equipmentName = '';
-		equipmentQuantity = 0;
+		equipmentQuantity = 1;
 	};
 
 	const removeEquipment = (equipment_i: EquipmentItem) => {
@@ -143,184 +143,190 @@
 
 <!-- FLOWBITE https://flowbite.com/docs/forms/search-input/ -->
 
-<div class="flex gap-2">
-	<div class="w-1/3 space-y-5">
-		<!-- Recipe info -->
-		<h1 class="text-lg font-bold">
+<div class="gap-2 w-1/3 space-y-8">
+  <!-- Recipe info -->
+  <article class="space-y-2">
+    <h1 class="text-lg font-bold">
       Set Recipe Info
     </h1>
-
-		<!-- https://v1.tailwindcss.com/components/forms -->
-		<form class="w-full max-w-lg">
-			<div class="-mx-3 mb-6 flex flex-wrap">
-				<div class="w-full px-3">
-					<label
+    
+    <!-- https://v1.tailwindcss.com/components/forms -->
+    <form class="w-full max-w-lg space-y-4">
+      <div class="flex flex-wrap">
+        <div class="w-full">
+          <label
             class="for-small-field"
             for="recipeName"
           >RECIPE NAME</label>
-					<input
-						id="recipeName"
-						class="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
-						type="text"
-						placeholder="Recipe name"
-						bind:value={recipeName}
-					/>
-				</div>
-			</div>
-
-			<div class="-mx-3 mb-6 flex flex-wrap">
-				<div class="mb-6 w-full px-3 md:mb-0 md:w-1/3">
-					<label
+          <input
+            id="recipeName"
+            class="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+            type="text"
+            placeholder="Recipe name"
+            bind:value={recipeName}
+          />
+        </div>
+      </div>
+  
+      <div class="flex flex-wrap">
+        <div class="w-1/3">
+          <label
             class="for-small-field"
             for="servings"
           >SERVINGS</label>
-					<input
-						id="servings"
-						class="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:bg-white focus:outline-none"
-						type="text"
-						placeholder="Servings"
-						bind:value={servings}
-					/>
-				</div>
-
-				<div class="w-full px-3 md:w-1/3">
-					<label
+          <input
+            id="servings"
+            class="small-text-field px-2 py-3 w-full"
+            type="text"
+            placeholder="Servings"
+            bind:value={servings}
+          />
+        </div>
+  
+        <div class="w-1/3">
+          <label
             class="for-small-field"
             for="price"
           >PRICE</label>
-					<input
-						id="price"
-						class="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
-						type="text"
-						placeholder="Price"
-						bind:value={price}
-					/>
-				</div>
-
-				<div class="w-full px-3 md:w-1/3">
-					<label
+          <input
+            id="price"
+            class="small-text-field px-2 py-3 w-full"
+            type="text"
+            placeholder="Price"
+            bind:value={price}
+          />
+        </div>
+  
+        <div class="w-1/3">
+          <label
             class="for-small-field"
             for="cookDifficulty"
           >DIFFICULTY</label>
-					<input
-						id="cookDifficulty"
-						class="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
-						type="text"
-						placeholder="Difficulty"
-						bind:value={cookDifficulty}
-					/>
-				</div>
-			</div>
-
-			<div class="-mx-3 mb-6 flex flex-wrap">
-				<div class="w-full px-3">
-					<label
+          <input
+            id="cookDifficulty"
+            class="small-text-field px-2 py-3 w-full"
+            type="text"
+            placeholder="Difficulty"
+            bind:value={cookDifficulty}
+          />
+        </div>
+      </div>
+  
+      <div class="-mx-3 mb-6 flex flex-wrap">
+        <div class="w-full px-3">
+          <label
             class="for-small-field"
             for="cookDifficulty"
           >INSTRUCTIONS</label>
-					<input
-						id="instructions"
-						class="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
-						type="text"
-						placeholder="Instructions"
-						bind:value={instructions}
-					/>
-				</div>
-			</div>
-		</form>
+          <input
+            id="instructions"
+            class="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+            type="text"
+            placeholder="Instructions"
+            bind:value={instructions}
+          />
+        </div>
+      </div>
+    </form>
+  </article>
 
-		<!-- Add to list of ingredients -->
-		<div class="space-y-7">
-			<form class="max-w-md space-y-5" on:submit|preventDefault={addIngredient}>
-				<h1 class="text-lg font-bold">
-          Add Ingredients
-        </h1>
-        
-				<div class="flex gap-x-5">
-					<div class="w-1/3">
-						<label
-              class="for-small-field"
-              for="ingredientName"
-            >INGREDIENT</label>
-						<select
-							class="block w-max appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:border-gray-500 focus:bg-zinc-100 focus:outline-none"
-							bind:value={currentIngredient}
-						>
-              <option disabled selected>Choose...</option>
-							{#each ingredientsDb as ingredient}
-								<option value={ingredient}>{ ingredient.ingredientName }</option>
-							{/each}
-						</select>
-					</div>
-
-					<div class="w-1/3">
-						<label
-              class="for-small-field"
-              for="ingredientQuantity"
-            >QUANTITY</label>
-            <input
-							id="ingredientQuantity"
-							type="text"
-							class="small-text-field px-4 py-3"
-							bind:value={ingredientQuantity}
-						/>
-					</div>
-
-          <div class="w-1/3">
-            <input
-              type="submit"
-              value="+"
-              class="submit-button w-1/3"
-            />
-          </div>
-				</div>
-			</form>
-
-			<!-- Display ingredients -->
-			<ul>
-				<li class="flex items-center align-text-bottom">
-					<label
-            class="for-small-field grow" for=""
-          >NAME</label>
-					<label
-            class="for-small-field grow"
-            for=""
-          >QUANTITY</label>
-				</li>
-				
-				{#each ingredients as ingredient}
-					<li class="flex items-center align-text-bottom">
-						<!-- <p class="pr-4">{ingredient.ingredientName}</p>-->
-						<input
-							id="ingredientName"
-							type="text"
-							class="small-text-field grow"
-							bind:value={ingredient.ingredientObject.ingredientName}
-            />
-						<input
-							id="ingredientQuantity"
-							type="text"
-							class="small-text-field grow"
-							bind:value={ingredient.ingredientQuantity}
-            />
-						<button
-							type="button"
-							class="py-0.3 mb-2 me-2 rounded-full bg-red-700 px-2 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-							on:click={() => removeIngredient(ingredient)}
-            >x</button>
-					</li>
-				{/each}
-			</ul>
-
-			<!-- Add to list of equipment -->
-			<h1 class="text-lg font-bold">
-        Add Equipment
+  <!-- Add to list of ingredients -->
+  <article class="w-full max-w-lg space-y-4">
+    <form class="max-w-md space-y-5" on:submit|preventDefault={addIngredient}>
+      <h1 class="text-lg font-bold">
+        Add Ingredients
       </h1>
-			<form
-        class="max-w-md flex gap-x-5"
-        on:submit|preventDefault={addEquipment}
-      >
-				<div class="w-1/3">
+
+      <div class="flex gap-x-4">
+        <div class="w-1/2">
+          <label
+            class="for-small-field"
+            for="ingredientName"
+          >INGREDIENT</label>
+          <select
+            class="block rounded px-4 py-1.5 text-gray-700 bg-gray-200 focus:border-gray-500 focus:outline-none leading-tight w-full"
+            placeholder="Choose..."
+            bind:value={currentIngredient}
+          >
+            <option disabled selected>Choose...</option>
+            {#each ingredientsDb as ingredient}
+              <option value={ingredient}>{ ingredient.ingredientName }</option>
+            {/each}
+          </select>
+        </div>
+
+        <div class="w-1/3">
+          <label
+            class="for-small-field w-max"
+            for="ingredientQuantity"
+          >QUANTITY</label>
+          <input
+            id="ingredientQuantity"
+            type="text"
+            class="small-text-field px-4 py-1.5 w-full"
+            placeholder="Quantity"
+            bind:value={ingredientQuantity}
+          />
+        </div>
+
+        <!-- <div class="h-max"> -->
+          <button
+            type="submit"
+            class="add-item w-max h-min"
+          >+</button>
+        <!-- </div> -->
+      </div>
+    </form>
+
+    <!-- Display ingredients -->
+    <ul>
+      <li class="flex align-text-bottom">
+        <label
+          class="for-small-field grow" for=""
+        >CURRENT INGREDIENTS</label>
+        <!-- <label
+          class="for-small-field grow" for=""
+        >QUANTITY</label> -->
+      </li>
+      
+      {#each ingredients as ingredient}
+        <li class="flex align-text-bottom gap-x-2">
+          <!-- <p class="pr-4">{ingredient.ingredientName}</p>-->
+          <input
+            id="ingredientName"
+            type="text"
+            class="small-text-field w-1/2 px-2"
+            bind:value={ingredient.ingredientObject.ingredientName}
+          />
+          <input
+            id="ingredientQuantity"
+            type="text"
+            class="small-text-field w-1/2 px-2"
+            bind:value={ingredient.ingredientQuantity}
+          />
+          <button
+            type="button"
+            class="remove-item"
+            on:click={() => removeIngredient(ingredient)}
+          >x</button>
+        </li>
+      {/each}
+      
+    </ul>
+  </article>
+
+  <!-- Add to list of equipment -->
+  <article class="w-full max-w-lg space-y-4">
+    <form
+      class="max-w-md space-y-5"
+      on:submit|preventDefault={addEquipment}
+    >
+      <h1 class="text-lg font-bold">
+        Add Ingredients
+      </h1>
+
+      <div class="flex gap-x-4">
+        <div class="w-1/2 gap-x-4">
           <label
             class="for-small-field"
             for="equipmentName"
@@ -328,11 +334,11 @@
           <input
             id="equipmentName"
             type="text"
-            class="small-text-field px-4 py-3"
+            class="small-text-field px-4 py-1.5 block leading-tight w-full"
+            placeholder="Equipment"
             bind:value={equipmentName}
-            />
+          />
         </div>
-
         <div class="w-1/3">
           <label
             class="for-small-field"
@@ -341,61 +347,58 @@
           <input
             id="equipmentQuantity"
             type="text"
-            class="small-text-field px-4 py-3"
+            class="small-text-field px-4 py-1.5 shrink w-full"
             bind:value={equipmentQuantity}
             />
         </div>
-
-				<input
+        <!-- <div class="w-1/3 flex align-items-center"> -->
+        <button
           type="submit"
-          value="+"
-          class="submit-button w-1/4"
-				  />
-			</form>
+          class="add-item h-min"
+        >+</button>
+      </div>
+      <!-- </div> -->
+    </form>
 
-			<!-- Display equipment -->
-			<ul>
-				<li class="flex items-center align-text-bottom">
-					<label
-            class="for-small-field" for=""
-          >EQUIPMENT NAME</label>
-					<label
-            class="for-small-field pl-20"
-            for=""
-          >QUANTITY</label>
-				</li>
+    <!-- Display equipment -->
+    <ul>
+      <li class="flex align-text-bottom">
+        <label
+          class="for-small-field grow" for=""
+        >CURRENT EQUIPMENT</label>
+        <!-- <label
+          class="for-small-field grow" for=""
+        >QUANTITY</label> -->
+      </li>
 
-        {#each equipment as equipment}
-          <li class="flex items-center align-text-bottom">
-            <!-- <p class="pr-4">{ingredient.ingredientName}</p>-->
-            <input
-              id="equipmentName"
-              type="text"
-              class="small-text-field"
-              bind:value={equipment.equipmentName}
-            />
-            <input
-              id="equipmentQuantity"
-              type="text"
-              class="small-text-field"
-              bind:value={equipment.equipmentQuantity}
-            />
-            <button
-              type="button"
-              class="py-0.3 mb-2 me-2 rounded-full bg-red-700 px-2 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-              on:click={() => removeEquipment(equipment)}
-            >x</button>
-          </li>
-        {/each}
-			</ul>
+      {#each equipment as equipment}
+        <li class="flex items-center align-text-bottom gap-x-2">
+          <!-- <p class="pr-4">{ingredient.ingredientName}</p>-->
+          <input
+            id="equipmentName"
+            type="text"
+            class="small-text-field px-2 w-1/2"
+            bind:value={equipment.equipmentName}
+          />
+          <input
+            id="equipmentQuantity"
+            type="text"
+            class="small-text-field px-2 w-1/2"
+            bind:value={equipment.equipmentQuantity}
+          />
+          <button
+            type="button"
+            class="remove-item"
+            on:click={() => removeEquipment(equipment)}
+          >x</button>
+        </li>
+      {/each}
+    </ul>
 
-			<!-- Submit recipe -->
-			<button
-				class="bg-main hover:bg-main-dark rounded border px-4 py-2 font-bold text-white"
-        on:click|preventDefault={handleCreateRecipe}
-      >Submit Recipe</button>
-		</div>
-		<!-- FLOWBITE https://flowbite.com/docs/forms/select/ -->
-	</div>
+    <!-- Submit recipe -->
+    <button
+      class="bg-main hover:bg-main-dark rounded border px-4 py-2 font-bold text-white"
+      on:click|preventDefault={handleCreateRecipe}
+    >Submit Recipe</button>
+  </article>
 </div>
-<!-- FLOWBITE https://flowbite.com/docs/components/tables/ -->
