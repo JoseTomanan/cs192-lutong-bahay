87808fa2033a779cd5b90f461616aeda2570715a
HoweZae <jdtomanan@up.edu.ph>
2025-04-08 17:07:31 +0800

    feat: Make delete recipe interface consistent with suspend user

--- frontend/src/lib/components/DeleteRecipe.svelte
+++ frontend/src/lib/components/DeleteRecipe.svelte
@@ -22,7 +22,7 @@
         method: 'POST',
         credentials: 'include',
         headers: {
-          'X-CSRFToken': getCookie("csrftoken"),
+          ...(getCookie("csrftoken") && { 'X-CSRFToken': getCookie("csrftoken") }),
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({recipeName})
@@ -42,18 +42,20 @@
 <Toaster/>
 
 <div class="max-w-md mt-6 p-6 bg-white rounded-lg border-2">
-  <h2 class="text-xl font-semibold mb-4">
-    Recipe Management
+  <h2 class="text-lg font-semibold mb-4">
+    Delete a recipe
   </h2>
   
   <div class="mb-4">
-    <label for="username" class="block text-sm font-medium text-gray-700 mb-1">Recipe Name</label>
+    <label for="username" class="block text-sm font-medium text-gray-700 mb-1">
+      Enter recipe name
+    </label>
     <input 
       id="username"
       type="text" 
       bind:value={recipeName}
       placeholder="Enter Recipe"
-      class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+      class="md-text-field"
     />
   </div>
   
@@ -61,7 +63,7 @@
     <button 
       on:click={deleteRecipe}
       class="destructive-text"
-    >Delete Recipe</button>
+    >Delete</button>
   </div>
   
 
