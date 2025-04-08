290c89d36b572d35273ed26cd47e67ecfb416c57
jijahh <eagalang@up.edu.ph>
2025-04-08 21:54:09 +0800

    fix(Backend): used IDs instead of names

--- backend/savedby/views.py
+++ backend/savedby/views.py
@@ -15,71 +15,53 @@ Get recipe, either all recipes or by search
 
 @api_view(["POST"])
 def user_save_recipe(request):
-    recipe_name = request.data.get("recipeName", None)
-    username = request.data.get("username", None)
-    temp = {}
-    if not recipe_name:
-        return Response({"error": "recipeName is required"}, status=400)
-
-    if not username:
-        return Response({"error": "username is required"}, status=400)
-
-    recipe = Recipe.objects.filter(recipeName=recipe_name).first()
-
+    recipeId = request.data.get("recipeId", None)
+    userId = request.data.get("userId", None)
+    if not recipeId:
+        return Response({"error": "recipeId is required"}, status=400)
+    if not userId:
+        return Response({"error": "userId is required"}, status=400)
+    recipe = Recipe.objects.get(pk=recipeId)
     if not recipe:
         return Response({"error": "Recipe not found"}, status=404)
-    user = User.objects.filter(username=username).first()
+    user = User.objects.get(pk=userId)
     if not user:
         return Response({"error": "User not found"}, status=404)
-    temp["recipe"] = recipe.id  # type: ignore
-    temp["user"] = user.id
-    saved_recipe = SavedBySerializer(data=temp)
+
+    saved_recipe = SavedBySerializer(data={"recipe": recipeId, "user": userId})
     if saved_recipe.is_valid():
         saved_recipe.save()
+        return Response({"message": "Recipe saved successfully"}, status=201)
     else:
-        return Response(saved_recipe.errors)
-
-    return Response({"message": "Recipe saved successfully"}, status=200)
+        return Response({"error": "Failed to save recipe"}, status=400)
 
 
 @api_view(["POST"])
 def user_delete_recipe(request):
-    recipe_name = request.data.get("recipeName", None)
-    username = request.data.get("username", None)
-    if not recipe_name:
-        return Response({"error": "recipeName is required"}, status=400)
+    recipeId = request.data.get("recipeId", None)
+    userId = request.data.get("userId", None)
+    if not recipeId:
+        return Response({"error": "recipeId is required"}, status=400)
+    if not userId:
+        return Response({"error": "userId is required"}, status=400)
 
-    if not username:
-        return Response({"error": "username is required"}, status=400)
-
-    recipe = Recipe.objects.filter(recipeName=recipe_name).first()
-
-    if not recipe:
-        return Response({"error": "Recipe not found"}, status=404)
-    user = User.objects.filter(username=username).first()
-    if not user:
-        return Response({"error": "User not found"}, status=404)
-
-    saved_recipe = SavedBy.objects.filter(recipe=recipe.id, user=user.id).first()
+    saved_recipe = SavedBy.objects.filter(recipe=recipeId, user=userId).first()
     if not saved_recipe:
         return Response({"error": "Saved recipe not found"}, status=404)
+
     saved_recipe.delete()
     return Response({"message": "Recipe deleted successfully"}, status=200)
 
 
 @api_view(["POST"])
 def user_get_saved_recipes(request):
-    username = request.data.get("username", None)
-    if not username:
-        return Response({"error": "username is required"}, status=400)
+    userId = request.data.get("userId", None)
+    if not userId:
+        return Response({"error": "userId is required"}, status=400)
 
-    user = User.objects.filter(username=username).first()
-    if not user:
-        return Response({"error": "User not found"}, status=404)
-
-    saved_recipes = SavedBy.objects.filter(user=user.id).values("recipe__recipeName")
+    saved_recipes = SavedBy.objects.filter(user=userId)
     if not saved_recipes:
-        return Response({"message": "No saved recipes found"}, status=200)
+        return Response({"message": "No saved recipes found"}, status=404)
 
-    recipes = [recipe["recipe__recipeName"] for recipe in saved_recipes]
-    return Response({"saved_recipes": recipes}, status=200)
+    serializer = SavedBySerializer(saved_recipes, many=True)
+    return Response(serializer.data, status=200)
