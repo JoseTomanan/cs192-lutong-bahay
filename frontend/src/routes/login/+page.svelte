6710b770b1dd348afb2b0f49155fb6478863a433
Zachary Michael Romualdez <zromualdez@gmail.com>
2025-05-06 13:55:31 +0800

    Update +page.svelte

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -56,6 +56,7 @@
         headers: {
           // 'X-CSRFToken': Cookies.get("csrftoken"),
           // 'X-CSRFToken': getCookie("csrftoken"),
+          'X-CSRFToken': csrfToken,
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ username, password })
